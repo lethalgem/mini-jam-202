8ce66eec3367b4299b80c6ee6e1aef1a5c021d0c
Michael Bass <littlebass09@neo.tamu.edu>
2026-01-17 15:16:08 -0800

    Hero can move and attack (#14)
    * MB Pulling Ian's changes
    
    * MB Started enemy
    
    * MB Enemy and enemy spawn working
    
    * MB Added initial tile mapping
    
    * MB Small change made to tile map
    
    * MB Player can move and attack
    
    * Update scripts/player_character_body_2d.gd
    
    Co-authored-by: lethalgem <iwc123@gmail.com>
    
    * Update scripts/player_character_body_2d.gd
    
    Co-authored-by: lethalgem <iwc123@gmail.com>
    
    ---------
    
    Co-authored-by: lethalgem <iwc123@gmail.com>

--- scripts/player_character_body_2d.gd
+++ scripts/player_character_body_2d.gd
@@ -7,16 +7,13 @@ class_name PlayerCharacterBody2D extends CharacterBody2D
 @export var idle_speed: float = 1.0
 @export var death_speed: float = 1.0
 @export var attack_cooldown: float = 1.0
+@export var speed := 300.0
 
 var death_bubble_controller_scene := preload("res://scenes/death_bubble_controller.tscn")
 
 func _ready():
 	idle()
 
-func _input(event):
-	if event is InputEventMouseButton and event.pressed:
-		if event.button_index == MOUSE_BUTTON_LEFT:
-			start_teleport(event.position)
 
 func start_teleport(location: Vector2):
 	animated_sprite_2D.play("teleport_start", teleport_speed)
@@ -29,7 +26,7 @@ func start_teleport(location: Vector2):
 func attack():
 	animated_sprite_2D.play("slash", attack_speed)
 	await animated_sprite_2D.animation_finished
-	die()
+	idle()
 
 func idle():
 	animated_sprite_2D.play("idle", idle_speed)
@@ -44,3 +41,26 @@ func die():
 	death_bubble_controller.scale = scale
 	death_bubble_controller.start()
 	queue_free()
+
+
+func _physics_process(delta):
+	var direction := 0
+
+	if Input.is_action_pressed("ui_left"):
+		direction -= 1
+		animated_sprite_2D.flip_h = true
+	if Input.is_action_pressed("ui_right"):
+		direction += 1
+		animated_sprite_2D.flip_h = false
+	
+	print(direction)
+
+	velocity.x = direction * speed
+	velocity.y = 0 
+
+	move_and_slide()
+	
+	if Input.is_action_just_pressed("attack"):
+		attack()
+	
+	
