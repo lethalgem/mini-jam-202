fbc09a205715d06e60d138c6ea4e766214d39e45
Michael Bass <littlebass09@neo.tamu.edu>
2026-01-17 14:46:51 -0800

    Added enemies and initial tile map (#12)
    * MB Pulling Ian's changes
    
    * MB Started enemy
    
    * MB Enemy and enemy spawn working
    
    * MB Added initial tile mapping
    
    * MB Small change made to tile map

---
+++ scripts/world_test.gd
@@ -0,0 +1,53 @@
+extends Node2D
+
+@onready var enemy_scene = preload("res://scenes/enemy_test.tscn")
+var spawn_points = [] # Array to hold possible spawn positions (Vector2)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+@export var min_spawn := 0.05
+@export var max_spawn := 0.3
+
+var spawn_timer: Timer
+
+func _ready():
+	randomize()
+	spawn_timer = Timer.new()
+	spawn_timer.autostart = true
+	spawn_timer.one_shot = false
+	spawn_timer.wait_time = randf_range(min_spawn, max_spawn)
+	spawn_timer.timeout.connect(_on_spawn_timeout)
+	add_child(spawn_timer)
+
+func _on_spawn_timeout():
+	var enemy := enemy_scene.instantiate() as Enemy
+	
+	var rect = get_viewport().get_visible_rect()
+	var randomValue = randf_range(.25, .75)
+
+	var spawn_x = rect.size.x * randomValue
+	var spawn_y = rect.position.y  # top of the screen
+
+	enemy.global_position = Vector2(spawn_x, spawn_y)
+	enemy.speed = randf_range(65, 350)
+	
+	#var scale_value = randf_range(0.7, 1.3)
+	var scale_value = randf_normal(1.3, 1)
+	while scale_value > 4:
+		scale_value = randf_normal(1.3, 1)
+	scale_value = max(scale_value, .7)
+	
+	enemy.scale = Vector2(scale_value, scale_value)
+	
+	get_parent().add_child(enemy)
+	
+	spawn_timer.wait_time = randf_range(min_spawn, max_spawn)
+	
+func randf_normal(mean := 1.0, std_dev := 0.15) -> float:
+	var u1 = randf()
+	var u2 = randf()
+	var z0 = sqrt(-2.0 * log(u1)) * cos(TAU * u2)
+	return mean + z0 * std_dev
