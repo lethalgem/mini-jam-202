a156ebfaa08d00cac67f751840ba1013972a22c2
ChapoTheGoat <gama_giovanni@yahoo.com>
2026-01-17 19:04:50 -0600

    UI Updates

---
+++ scripts/counters_ui.gd
@@ -0,0 +1,13 @@
+class_name CountersUI extends Control
+
+@export var kill_counter_label : Label
+@export var time_label : Label
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,17 +1,23 @@
 class_name GameManager extends Node2D
 
+@export var counter_ui : CountersUI
+
 var time_since_time_update: float = 0
 var total_time_survived_sec: float = 0
 
 func _physics_process(delta: float):
 	time_since_time_update += delta
 	if time_since_time_update >= 1.0:
-		total_time_survived_sec += time_since_time_update
+		total_time_survived_sec += 1.0
 		update_time_survived()
 		time_since_time_update = 0
 
 func update_time_survived():
-	var time_survived_min = total_time_survived_sec / 60
-	var remaining_time_survived_sec = fmod(total_time_survived_sec, 60)
-	
+	var time_survived_min : int = total_time_survived_sec / 60
+	var remaining_time_survived_sec : int = fmod(total_time_survived_sec, 60)
 	# TODO update ui here
+	
+	if time_survived_min >= 1:
+		counter_ui.time_label.text ="Time Survived " + str(time_survived_min) + ":" + str(remaining_time_survived_sec)
+	else: 
+		counter_ui.time_label.text ="Time Survived " + str(remaining_time_survived_sec)
