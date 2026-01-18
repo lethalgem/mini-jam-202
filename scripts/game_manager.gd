dbecf90a1ee6a94cd787c60c9a3e3e186616fb08
ChapoTheGoat <gama_giovanni@yahoo.com>
2026-01-18 13:08:56 -0600

    UI Updates

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -2,9 +2,16 @@ class_name GameManager extends Node2D
 
 @export var counter_ui : CountersUI
 
+
+@onready var main_menu = $CanvasLayer2
+@onready var option_menu = $CanvasLayer4
+
 var time_since_time_update: float = 0
 var total_time_survived_sec: float = 0
 
+func _ready() -> void:
+	main_menu.visible = true
+	
 func _physics_process(delta: float):
 	time_since_time_update += delta
 	if time_since_time_update >= 1.0:
@@ -21,3 +28,14 @@ func update_time_survived():
 		counter_ui.time_label.text ="Time Survived " + str(time_survived_min) + ":" + str(remaining_time_survived_sec)
 	else: 
 		counter_ui.time_label.text ="Time Survived " + str(remaining_time_survived_sec)
+
+func _on_title_option_but_pressed() -> void:
+	print("signal for option menu pressed working")
+	option_menu.visible = true
+	main_menu.visible = false
+
+
+func _on_option_menu_back_button_pressed() -> void:
+	print("back button pressed")
+	option_menu.visible = false
+	main_menu.visible = true
--- scripts/title.gd
+++ scripts/title.gd
@@ -1,30 +1,30 @@
-extends Control
+class_name TitleScreen extends Control
 
 @export var title_label: Label
 @export var start_button: Button
 @export var button_sound : AudioStreamPlayer
 
+signal option_but_pressed
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	await get_tree().process_frame
 	play_floating_animation()
-	
-	
 func play_floating_animation() -> void: 
 	var tween := create_tween()
-	
+
 	var position_offest := Vector2(0.0,10.0)
 	var duration := 1.0
 	tween.tween_property(title_label, "position", position_offest, duration)
 	tween.tween_property(title_label, "position", -1.0*position_offest, duration)
 	
 	tween.set_loops()
-#
+
 func _on_start_button_pressed() -> void:
-	print("button clicked")
 	button_sound.play()
-##
+
 #func start_pressed_animation() -> void:
-	#print("Start button flashing works")
+	#print("pressed start game")
 	#var tween := create_tween()
 	#var hover_color = start_button.get_theme_color("font_hover_color")
 	#var pressed_color = start_button.get_theme_color('font_focus_color')
@@ -32,3 +32,7 @@ func _on_start_button_pressed() -> void:
 	#tween.tween_property(start_button,"theme_override_colors/font_focus_color",pressed_color,duration).set_trans(Tween.TRANS_BOUNCE)
 	#tween.tween_property(start_button,"theme_override_colors/font_focus_color",hover_color,duration).set_trans(Tween.TRANS_BOUNCE)
 	#tween.set_loops()
+
+func _on_option_button_pressed() -> void:
+	option_but_pressed.emit()
+	button_sound.play()
