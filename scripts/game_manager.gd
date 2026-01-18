471be1870245651b5eb4f6aac140fce645529d1a
lethalgem <iwc123@gmail.com>
2026-01-18 14:30:55 -0500

    Fix warnings

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -20,8 +20,8 @@ func _physics_process(delta: float):
 		time_since_time_update = 0
 
 func update_time_survived():
-	var time_survived_min : int = total_time_survived_sec / 60
-	var remaining_time_survived_sec : int = fmod(total_time_survived_sec, 60)
+	var time_survived_min : int = int(total_time_survived_sec / 60)
+	var remaining_time_survived_sec : int = int(fmod(total_time_survived_sec, 60))
 	
 	if time_survived_min >= 1:
 		counter_ui.time_label.text ="Time Survived " + str(time_survived_min) + ":" + str(remaining_time_survived_sec)
